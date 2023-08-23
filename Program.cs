1aeb0128cef11b42deaf0f002f6c16f498886d7d
MarufUsmonovdev <marufusmonov1996@gmail.com>
2023-08-24 04:43:08 +0500

    INFO: Change

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// NAME
-System.Console.WriteLine("Enter your name: ");
-string name = System.Console.ReadLine();
+System.Console.WriteLine($"Enter your name: ");
+string name = Console.ReadLine();
 string greeting = $"Hello, {name}";
 System.Console.WriteLine(greeting);
 
@@ -10,7 +10,11 @@ string ownage = Console.ReadLine();
 System.Console.WriteLine($"Converting... ");
 int age = Convert.ToInt32(ownage);
 System.Console.WriteLine($"Seccesfully converted {age}");
-int Keschasage = 2;
-int differenceAge = age - Keschasage;
+int KeschaAge = 2;
+int differenceAge = age - KeschaAge;
 System.Console.WriteLine($"The difference between your and Kescha's age is {differenceAge}");
 
+
+System.Console.WriteLine($"You are older than Kescha's age: {differenceAge > 0}");
+System.Console.WriteLine($"You are younger than Kescha's age: {differenceAge < 0}");
+System.Console.WriteLine($"Your age the same with Kescha: {differenceAge == 0}");
