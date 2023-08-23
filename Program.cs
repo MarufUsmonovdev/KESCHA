dfe7f16bc42a5b391d3cf02f778b7d55dc1b7d5b
MarufUsmonovdev <marufusmonov1996@gmail.com>
2023-08-23 17:54:32 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// NAME
+System.Console.WriteLine("Enter your name: ");
+string name = System.Console.ReadLine();
+string greeting = $"Hello, {name}";
+System.Console.WriteLine(greeting);
+
+//AGE
+System.Console.WriteLine("Enter your age: ");
+string ownage = Console.ReadLine();
+System.Console.WriteLine($"Converting... ");
+int age = Convert.ToInt32(ownage);
+System.Console.WriteLine($"Seccesfully converted {age}");
+int Keschasage = 2;
+int differenceAge = age - Keschasage;
+System.Console.WriteLine($"The difference between your and Kescha's age is {differenceAge}");
+
